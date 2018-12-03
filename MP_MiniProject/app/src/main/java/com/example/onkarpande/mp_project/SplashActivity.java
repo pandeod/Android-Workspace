3d376ffd9432254435531884d91a99ea4855a6fe
Onkar Pande <onkardpande@gmail.com>
2018-12-04 00:23:54 +0530

    version update

--- MP_MiniProject/app/src/main/java/com/example/onkarpande/mp_project/SplashActivity.java
+++ MP_MiniProject/app/src/main/java/com/example/onkarpande/mp_project/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.onkarpande.mp_project.Activity.HomeActivity;
+
 public class SplashActivity extends AppCompatActivity {
 
     private static int STATIC_TIME_OUT=3000;
@@ -15,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-               startActivity(new Intent(getApplicationContext(),LogInActivity.class));
+               startActivity(new Intent(getApplicationContext(),HomeActivity.class));
 
                //built in android fade in-out animation
                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
